5bf9d172425efce666162e3dcc0ad98fbd5f8331
Qian <161250102@smail.nju.edu.cn>
2018-11-02 15:15:08 +0800

    update code

--- code-v2.cpp
+++ code-v2.cpp
@@ -12,11 +12,25 @@ using namespace std;
 using namespace GPIO;
 
 #define NOISE_MAX_AREA 500
-#define SPEED 50
+#define SPEED 5
 //Uncomment this line at run-time to skip GUI rendering
 #define _DEBUG
 
 const string CAM_PATH="/dev/video0";
+
+//获取直线在图像大小顶部/底部的截点，存入vector数组
+vector<Point> getEdgePoint(int w, int h, Vec4f& line) {
+	double k = line[1] / line[0];
+	Point p1, p2;
+	p1.x = line[2] - line[3] / k;
+	p1.y = 0;
+	p2.x = line[2] + (h - line[3]) / k;
+	p2.y = h;
+	vector<Point> result;
+	result.push_back(p1);
+	result.push_back(p2);
+	return result;
+}
 
 /*
 * 获取小车当前位置
@@ -49,6 +63,7 @@ int getPosition(Mat& image, vector<Mat>* image_vector) {
 	drawContours(binary_img, contours, -1, Scalar::all(255));
 	
 	vector<Vec4f> lines;
+	double k_sum = 0.0;
 	for (int i = 0; i < contours.size(); i++) {
 		vector<Point> contours_poly;
 		//计算凸包
@@ -60,19 +75,22 @@ int getPosition(Mat& image, vector<Mat>* image_vector) {
 			Vec4f lineData;
 			//将轮廓拟合为直线
 			fitLine(contours[i], lineData, DIST_L2, 0, 1e-2, 1e-2);
+			k_sum += (double) lineData[1] / lineData[0];
 			lines.push_back(lineData);
+			vector<Point> line_point = getEdgePoint(width, height, lineData);
+			line(image, line_point[0], line_point[1], Scalar(0, 0, 255), 3);
 		}
-	
+	}
 	image_vector->push_back(image);
 	image_vector->push_back(binary_img);
 	
 	if (lines.size() == 0) {
 		return -1; //没有直线——应该到终点了吧
-	} else if (lines.size() > 2) {
+	} else if (lines.size() > 3) {
 		return 0; //直线过多——应该还在路上
 	} else {
 		//区分一下左和右
-		return (lines[0][1] / lines[0][0]) > 0 ? 1 : 2; // 1-方向偏右，向左调整；2-方向偏左，向右调整
+		return k_sum > 0 ? 1 : 2; // 1-方向偏右，向左调整；2-方向偏左，向右调整
 	}
 	return 0;
 }
@@ -89,10 +107,10 @@ void control(int pos) {
 			turnTo(0);
 			break;
 		case 1:
-			turnTo(10);
+			turnTo(-15);
 			break;
 		case 2:
-			turnTo(-10);
+			turnTo(15);
 			break;
 		default:
 			stopLeft();
@@ -109,7 +127,7 @@ int main() {
 	clog << "Frame Size: " << dWidth << "x" << dHeight << endl;
 
 	Mat image;
-
+	init();
 	controlLeft(FORWARD, SPEED);
 	controlRight(FORWARD, SPEED);
 	while(true) {
@@ -117,15 +135,15 @@ int main() {
 		if(image.empty())
 			break;
 		
-		Rect roi(0, image.rows/2, image.cols, image.rows/4);
+		Rect roi(0, image.rows/2, image.cols, image.rows*3/8);
 		Mat imgROI = image(roi);
 		vector<Mat>* results = new vector<Mat>();
 		
 		int n = getPosition(imgROI, results);
+		cout << n << endl;
 		control(n);
 		
 		#ifdef _DEBUG
-		cout << n << endl;
 		for(int i = 0; i < results->size(); i++) {
 			imshow("Image " + i, (*results)[i]);
 		}
